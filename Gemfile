4c2102d7206bce0d621b88ec340ab24f1935ad2e
Jamie Wright <jamie@brilliantfantastic.com>
2011-09-28 00:33:23 -0400

    HIRB for printing tickets

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'httparty', '0.7.8'
 gem 'json', '1.4.6'
 gem 'crack', '0.1.8'
 gem 'thor', '0.14.6'
+gem 'hirb'
 
 group :test do
   gem 'diff-lcs', '1.1.3'
--- lib/morale/commands/ticket.rb
+++ lib/morale/commands/ticket.rb
@@ -1,6 +1,7 @@
 require 'morale/client'
 require 'morale/command'
 require 'morale/authorization'
+require 'hirb'
 
 module Morale::Commands
   class Ticket
@@ -8,7 +9,27 @@ module Morale::Commands
       include Morale::Platform
       
       def command(command)
-        Morale::Command.client.ticket Morale::Account.project, command
+        ticket = Morale::Command.client.ticket Morale::Account.project, command
+        print ticket
+      end
+      
+      private
+      
+      def print(ticket)
+        due_date = Date.parse(ticket['due_date']).strftime("%b. %d") unless ticket['due_date'].nil?
+        assigned_to = "#{ticket['assigned_to']['user']['first_name']} #{(ticket['assigned_to']['user']['last_name']).to_s[0,1]}" unless ticket['assigned_to'].nil?
+        
+        say Hirb::Helpers::Table.render [{
+          :id => ticket['identifier'], 
+          :type => ticket['type'],
+          :title => ticket['title'],
+          :created_by => "#{ticket['created_by']['user']['first_name']} #{(ticket['created_by']['user']['last_name']).to_s[0,1]}",
+          :due_date => due_date,
+          :assigned_to => assigned_to,
+          :priority => ticket['priority']
+          }], 
+          :fields => [:id, :type, :title, :created_by, :due_date, :assigned_to, :priority],
+          :headers => { :created_by => "created by", :due_date => "due date", :assigned_to => "assigned to" }
       end
     end
   end
